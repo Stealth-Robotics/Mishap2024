03bcbad8c640229ef1f7ae3734431fefd76b55b1
4089Software <stealthrobotics@gmail.com>
2024-09-30 16:29:36 -0700

    fixed THE SCREAMING!!!!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.button.Button;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import org.firstinspires.ftc.teamcode.subsystems.SimpleMecanumDriveSubsystem;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.stealthrobotics.library.Alliance;
+import org.stealthrobotics.library.opmodes.StealthOpMode;
+
+public abstract class Teleop extends StealthOpMode {
+
+    // Subsystems
+    SimpleMecanumDriveSubsystem drive;
+
+    // Game controllers
+    GamepadEx driveGamepad;
+    GamepadEx mechGamepad;
+
+
+    @Override
+    public void initialize() {
+        // Setup and register all of your subsystems here
+        drive = new SimpleMecanumDriveSubsystem(hardwareMap);
+
+        register(drive);
+
+        driveGamepad = new GamepadEx(gamepad1);
+        mechGamepad = new GamepadEx(gamepad2);
+
+        // Automatically reset the elevator all the way down when we init
+//        schedule(new ResetElevatorCommand(elevator));
+
+        // A subsystem's default command runs all the time. Great for drivetrains and such.
+        drive.setDefaultCommand(
+                new DefaultMecanumDriveCommand(
+                        drive,
+                        () -> driveGamepad.gamepad.left_stick_y,
+                        () -> driveGamepad.gamepad.left_stick_x,
+                        () -> driveGamepad.gamepad.right_stick_x
+                )
+        );
+
+        Button button = mechGamepad.getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whenPressed(
+                new SequentialCommandGroup(
+                        new InstantCommand(() -> elevator.setTargetLocation(0.0)),
+                        new InstantCommand(() -> arm.scorePosition())
+                )
+        );
+        Button button1 = mechGamepad.getGamepadButton(GamepadKeys.Button.DPAD_UP).whenPressed(
+                new SequentialCommandGroup(
+                        new InstantCommand(() -> elevator.setTargetLocation(1)),
+                        new InstantCommand(() -> arm.intakePosition())
+                )
+        );
+
+        mechGamepad.getGamepadButton(GamepadKeys.Button.BACK).whenPressed(new ResetElevatorCommand(elevator));
+        driveGamepad.getGamepadButton(GamepadKeys.Button.Y).whenPressed(new InstantCommand(() -> drive.resetHeading()));
+
+        mechGamepad.getGamepadButton(GamepadKeys.Button.X).whenPressed(new InstantCommand(() -> lever.toggle()));
+        mechGamepad.getGamepadButton(GamepadKeys.Button.Y).whenPressed(new InstantCommand(() -> arm.toggle()));
+        mechGamepad.getGamepadButton(GamepadKeys.Button.A).whenPressed(new InstantCommand(() -> airplane.toggle()));
+
+
+    }
+
+    /**
+     * Ideally your red vs. blue opmodes are nothing more than this. Keep code shared between
+     * them, and take different actions based on the alliance color.
+     *
+     * @see org.stealthrobotics.library.Alliance
+     */
+
+    @SuppressWarnings("unused")
+    @TeleOp(name = "RED | Tele-Op", group = "Red")
+    public static class RedTeleop extends Teleop {
+    }
+
+    @SuppressWarnings("unused")
+    @TeleOp(name = "BLUE | Tele-Op", group = "Blue")
+    public static class BlueTeleop extends Teleop {
+    }
+
+    @Override
+    public double getFinalHeading() {
+        return drive.getHeading();
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SimpleMecanumDriveSubsystem.java
@@ -0,0 +1,161 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import static org.stealthrobotics.library.opmodes.StealthOpMode.telemetry;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.stealthrobotics.library.AutoToTeleStorage;
+
+/**
+ * This is the most basic Mecanum subsystem you can have, and provides simple methods to drive and stop.
+ */
+public class SimpleMecanumDriveSubsystem extends SubsystemBase {
+    public final DcMotor leftFrontDrive;
+    public final DcMotor leftRearDrive;
+    public final DcMotor rightFrontDrive;
+    public DcMotor rightRearDrive;
+    final IMU imu;
+    boolean fieldcentric = true;
+    boolean slowMode = false;
+    double headingOffset = 0.0;
+
+    public SimpleMecanumDriveSubsystem(HardwareMap hardwareMap) {
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+        leftRearDrive = hardwareMap.get(DcMotor.class, "leftRearDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+        rightRearDrive = hardwareMap.get(DcMotor.class, "rightRearDrive");
+
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftRearDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightRearDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftRearDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRearDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Retrieve the IMU from the hardware map
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                        RevHubOrientationOnRobot.UsbFacingDirection.RIGHT
+                )
+        );
+
+        imu.initialize(parameters);
+
+    }
+
+    /**
+     * Drive using gamepad inputs. This includes compensating for imperfect strafing, and adjusting
+     * inputs based on stick directions. Better versions would include field-centric driving,
+     * deadbands, and more.
+     */
+    public void driveTeleop(double leftSickY, double leftStickX, double rightStickX) {
+        // This code is pulled from Game Manual 0
+        // https://gm0.org/en/latest/docs/software/mecanum-drive.html
+
+        double y = -leftSickY; // Remember, this is reversed!
+        double x = leftStickX * 1.1; // Counteract imperfect strafing
+        double rotation = rightStickX;
+        if (fieldcentric) {
+            // Read inverse IMU heading, as the IMU heading is CW positive
+            double botHeading = -getHeading();
+            double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+            double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+            x = rotX;
+            y = rotY;
+        }
+        y = y * Math.abs(y);
+        x = x * Math.abs(x);
+        rotation = rotation * Math.abs(rotation);
+
+        drive(y * 0.85, x * 0.85, rotation * 0.85);
+
+    }
+
+    public void toggleFieldCentric() {
+        fieldcentric = !fieldcentric;
+    }
+
+//    public double getHeading() {
+//        return -imu.getAngularOrientation().firstAngle;
+//    }
+
+    // The actual heading from the IMU, only adjusted so that positive is clockwise
+    public double getRawHeading() {
+        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+    }
+
+    // The heading we'll use to drive the bot, adjusted for an offset which we can set any time
+    // we want to correct for gyro drift as we drive.
+    public double getHeading() {
+        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS) - headingOffset;
+    }
+
+    // Adjust our heading so the front of the bot is forward, no matter how we've drifted over time.
+    public void resetHeading() {
+        headingOffset = getRawHeading();
+        AutoToTeleStorage.finalAutoHeading = 0;
+    }
+    public void drive(double y, double x, double rotation) {
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rotation), 1);
+        double leftFrontDrivePower = (y + x + rotation) / denominator;
+        double leftRearDrivePower = (y - x + rotation) / denominator;
+        double rightFrontDrivePower = (y - x - rotation) / denominator;
+        double rightRearDrivePower = (y + x - rotation) / denominator;
+
+        if(slowMode){
+            leftFrontDrivePower *= 0.5;
+            leftRearDrivePower *= 0.5;
+            rightFrontDrivePower *= 0.5;
+            rightRearDrivePower *= 0.5;
+        }
+
+        leftFrontDrive.setPower(leftFrontDrivePower);
+        leftRearDrive.setPower(leftRearDrivePower);
+        rightFrontDrive.setPower(rightFrontDrivePower);
+        rightRearDrive.setPower(rightRearDrivePower);
+    }
+
+    public void stop(){
+        leftFrontDrive.setPower(0);
+        leftRearDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightRearDrive.setPower(0);
+    }
+
+    public int getTicks(){
+        return leftFrontDrive.getCurrentPosition();
+    }
+
+    @Override
+    public void periodic() {
+//        telemetry.addData("Drive ticks", getTicks());
+        //      telemetry.addData("Field centric driving", fieldcentric);
+        //    telemetry.addData("Bot Heading", getHeading());
+        // telemetry.addData("leftfrontpower", leftFrontDrive.getPower());
+        // telemetry.addData("leftrearpower", leftRearDrive.getPower());
+        // telemetry.addData("rightfrontpower", rightFrontDrive.getPower());
+        // telemetry.addData("rightrearpower", rightRearDrive.getPower());
+
+    }
+    public void toggleSlowMode(){
+        slowMode = !slowMode;
+    }
+}
