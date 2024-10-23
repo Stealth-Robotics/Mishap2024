752abae161afe93a6878cf628ad062d358ebbd5e
able and <90404118+AbelAnand@users.noreply.github.com>
2024-10-23 16:33:16 -0700

    bucket is done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/BucketSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/BucketSubsystem.java
@@ -1,18 +1,31 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class BucketSubsystem extends SubsystemBase {
-    private final Servo bucketMotor;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-    public BucketSubsystem(HardwareMap hardwareMap) {
-        bucketMotor = hardwareMap.get(Servo.class, "bucket");
+public class BucketSubsystem extends SubsystemBase {
+    private final ServoEx bucketServo;
+
+    private final Telemetry telemetry;
+
+    public BucketSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
+        bucketServo = hardwareMap.get(ServoEx.class, "bucket");
+
+        this.telemetry = telemetry;
     }
 
     public void setPosition(double position) {
-        bucketMotor.setPosition(position);
+        bucketServo.setPosition(position);
     }
+
+    @Override
+    public void periodic() {
+        telemetry.addData("bucket", bucketServo.getPosition());
+    }
+
 }
